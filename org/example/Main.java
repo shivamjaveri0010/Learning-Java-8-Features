58a48782a747507225b15d1ad7b8ea0f2ee72e45
shivamjaveri0010 <shivamjaveri0010@gmail.com>
2026-04-07 20:24:14 +0530

    Day-33 of Task-18 Java 8+ Features (Update --> Creating thread with Lambda Expression)

--- org/example/Main.java
+++ org/example/Main.java
@@ -1,10 +1,20 @@
 package org.example;
 
 /*----------Task-18----------*/
+//adding feature of thread
 
 public class Main {
     public static void main(String[] args) {
         Employee obj = () -> "Software Engineer";
-        System.out.println(obj.getName());      
+        System.out.println(obj.getName());   
+        
+        Runnable rn = () -> {
+            for (int i=1; i<=10; i++) {
+                System.out.println("Hello " + i);
+            }
+        };
+
+        Thread thread = new Thread(rn);
+        thread.run();
     }
 }
