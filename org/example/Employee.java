1273b7e524b06a21148322803695509e51693756
shivamjaveri0010 <shivamjaveri0010@gmail.com>
2026-04-07 20:02:48 +0530

    Day-33 Task-18 of Java 8+ Features (Lambda Expression & Interface)

---
+++ org/example/Employee.java
@@ -0,0 +1,7 @@
+package org.example;
+
+/*----------Task-18----------*/
+
+public interface Employee {
+    String getName();
+}
---
+++ org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+/*----------Task-18----------*/
+
+public class Main {
+    public static void main(String[] args) {
+        Employee obj = () -> "Software Engineer";
+        System.out.println(obj.getName());      
+    }
+}
